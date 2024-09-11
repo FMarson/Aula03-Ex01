c8c9de9bd98e51ef20a07db686dc716b1d57ed3d
Fernando Marson <fernando.marson@gmail.com>
2024-09-11 18:20:03 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+1. Classe Livro
+
+Atributos:
+
+- String titulo
+- String autor
+- String ISBN
+- int numeroPaginas
+- double preco
+
+Construtores:
+
+- Construtor padrão
+- Construtor com todos os atributos
+- Construtor com titulo e autor
+
+Métodos:
+
+- Getters e Setters para todos os atributos
+- public void aplicarDesconto(double percentual)
+- public String getDescricao()
+- public boolean estaDentroOrcamento(double preco)
+*/
+
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+
+    
+  }
+}
